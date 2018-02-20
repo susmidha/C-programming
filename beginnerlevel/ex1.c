79179ddeef83f6d1371787d6cbc9ebdb6ce2adc0
susmidha <36623596+susmidha@users.noreply.github.com>
2018-02-20 10:23:04 +0530

    Create ex1.c

---
+++ beginnerlevel/ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n%2==0)
+printf("its even number");
+else
+printf("its odd number");
+	return 0;
+}
