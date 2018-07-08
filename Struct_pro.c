bb85634877258a0aa65116aadbfc034512737821
susmidha <36623596+susmidha@users.noreply.github.com>
2018-07-07 23:16:29 -0700

    Create Struct_pro.c

---
+++ Struct_pro.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+struct student{
+	int roll_no;
+	char name[20];
+}s1,s2,sum;
+int main(void) {
+	// your code goes here
+scanf("%d",&s1.roll_no);
+scanf("%d",&s2.roll_no);
+scanf("%s",&s1.name);
+scanf("%s",&s2.name);
+sum.roll_no=s1.roll_no+s2.roll_no;
+printf("%s",s1.name);
+printf("%s",s2.name);
+printf("%d",sum.roll_no);
+
+
+}
